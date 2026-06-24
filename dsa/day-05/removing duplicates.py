cdb779184507684516f160b1cf5faddb0e7d9da4
Duzzy5 <145280752+Duzzy5@users.noreply.github.com>
2026-06-24 23:24:03 +0530

    Create removing duplicates.py

---
+++ dsa/day-05/removing duplicates.py
@@ -0,0 +1,16 @@
+arr = [1,1,2,2,2,3,3,4]
+
+def remove_duplicates(arr):
+
+    i = 0
+
+    for j in range(1, len(arr)):
+
+        if arr[j] != arr[i]:
+            i += 1
+            arr[i] = arr[j]
+
+    return i + 1
+
+print(remove_duplicates(arr))
+print(arr)
