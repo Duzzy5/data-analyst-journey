d9e19d3424ec656bfe3bfd61cb832e12533aea34
Duzzy5 <145280752+Duzzy5@users.noreply.github.com>
2026-06-26 15:28:12 +0530

    Update two sums - 2.py

--- dsa/day-08/two sums - 2.py
+++ dsa/day-08/two sums - 2.py
@@ -1,1 +1,14 @@
+#two sums
+arr = [3, 2, 4]
+target = 6
 
+def ts(arr,target):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i] + arr[j] == target :
+                return i,j
+            else:
+                pass
+    return "Not Found"
+
+ts(arr,target)
