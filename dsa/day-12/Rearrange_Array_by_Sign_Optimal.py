8a0aca79a4d373d4818e0278ca73346e3aad89b6
Duzzy5 <145280752+Duzzy5@users.noreply.github.com>
2026-07-04 11:32:07 +0530

    Create Rearrange_Array_by_Sign_Optimal.py

---
+++ dsa/day-12/Rearrange_Array_by_Sign_Optimal.py
@@ -0,0 +1,22 @@
+arr = [3,1,-2,-5,2,-4]
+
+def rearrange(arr):
+
+    ans = [0] * len(arr)
+
+    pos = 0
+    neg = 1
+
+    for num in arr:
+
+        if num > 0:
+            ans[pos] = num
+            pos += 2
+        else:
+            ans[neg] = num
+            neg += 2
+
+    return ans
+
+
+print(rearrange(arr))
